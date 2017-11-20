7038e6157bf4a93e3cea7ad440478b6dd3c15bad
Ryan Baxter <rbaxter@pivotal.io>
2017-11-20 07:51:10 -0500

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
